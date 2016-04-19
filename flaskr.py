6c736ca666143ccf0f94009ce6e4327eada47738
Jessy Hurbain <JessyHurbain@gmail.com>
2016-04-19 11:52:10 +0200

    add def login()

--- flaskr.py
+++ flaskr.py
@@ -63,5 +63,22 @@ def add_entry():
     flash('New entry was successfully posted') #display new entry successfully
     return redirect(url_for('show_entries'))
 
+#Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == "POST":
+        #compare username and password
+        if request.form['username'] != app.config['USERNAME']:
+            error = "Invalid username"
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid password"
+        else:
+            session['logged_in'] = True
+            flash('You were logged_in') #display You were logged_in
+            return redirect(url_for('show_entries'))
+        return render_template('login.html', error=error)
+
+
 if __name__ == '__main__':
     app.run(debug = True)
