e1ea704eb365ae7e84f144e9ee465fb2b171dd53
Jessy Hurbain <JessyHurbain@gmail.com>
2016-04-19 11:04:28 +0200

    add app.route('/') and def show_entries()

--- flaskr.py
+++ flaskr.py
@@ -46,5 +46,11 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc') #select title, text in the database
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
 if __name__ == '__main__':
     app.run(debug = True)
