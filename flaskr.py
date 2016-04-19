44e48b82a4673b01689a410c74b0ac2280c9b5c1
Jessy Hurbain <JessyHurbain@gmail.com>
2016-04-19 12:21:19 +0200

    add templates layout

--- flaskr.py
+++ flaskr.py
@@ -81,10 +81,10 @@ def login():
 
 #logout
 @app.route('/logout')
-    def logout():
-        session.pop('logged_in', None)
-        flash('You were logged out')
-        return redirect(url_for('show_entries'))
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
     app.run(debug = True)
